2703958ae5771555273e3fbcd971f578444b2bbb
maker2413 <maker2413@gmail.com>
2022-08-27 01:06:06 -0500

    got podman testing working with apt and dnf

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -6,6 +6,24 @@ cd "${0%/*}";
 
 : "${CONTAINER_PORT:=80}"
 
+function wait_for_bootstrap() {
+  local __package=$1;
+  local found=0;
+  local timer=0;
+
+  while [[ "$found" == 0 && "$timer" < 600 ]]; do
+    echo "Waiting for bootstrap to complete...";
+
+    sleep 5;
+
+    if podman exec -it -e package=$__package yacm-$TEST-test /bin/bash -c 'which $package'; then
+      found=1;
+    fi
+
+    timer+=5;
+  done
+}
+
 podman build \
        -t yacm-$TEST ./$TEST;
 
@@ -21,17 +39,23 @@ case "$TEST" in
            yacm-$TEST-test \
            /root/run-yacm.sh;
 
-    echo "Waiting for 2 seconds to give httpd time to boot";
-    sleep 2;
+    echo -e "\nWaiting for 10 seconds to give httpd time to boot";
+    sleep 10;
 
     curl localhost:9090 || podman stop yacm-$TEST-test;;
   apt)
-    echo "Waiting for 20 seconds to give httpd time to boot";
-    sleep 20;
+    wait_for_bootstrap apache2;
 
     podman exec -it \
            yacm-$TEST-test \
            /bin/bash -c "which apache2" \
+      || podman stop yacm-$TEST-test;;
+  dnf)
+    wait_for_bootstrap httpd;
+
+    podman exec -it \
+           yacm-$TEST-test \
+           /bin/bash -c "which httpd" \
       || podman stop yacm-$TEST-test;;
 esac
 
